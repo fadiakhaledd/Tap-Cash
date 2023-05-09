d11f60db7c7a4147104007325afe8a1d7b350afd
fadiakhaled <110059717+fadiakhaledd@users.noreply.github.com>
2023-05-09 20:18:12 +0300

    Update getVirtualCreditCardController.js

--- controllers/VirtualCreditCardsController/getVirtualCreditCardController.js
+++ controllers/VirtualCreditCardsController/getVirtualCreditCardController.js
@@ -1,13 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import { UserRepository } from '../../repositories/UserRepository.js'
-import { TransactionRepository } from '../../repositories/TransactionRepository.js'
 import { VccRepository } from '../../repositories/VirtualCreditCardRepository.js'
 
 import { fetchCCData } from '../../services/CreditCardServices.js'
 
 let prisma = new PrismaClient()
 const userRepository = new UserRepository(prisma);
-const transactionRepository = new TransactionRepository(prisma);
 const vccRepository = new VccRepository(prisma);
 
 export async function getVCC(req, res) {
@@ -15,12 +13,15 @@ export async function getVCC(req, res) {
     try {
         const { visa_type, amount, userId } = req.body
 
+        // Check the user's existance
         const user = await userRepository.findUserByID(userId);
 
         if (!user) {
-            return res.status(400).json({ message: "User ID doesn't exist" })
+            return res.status(400).json({ error: "User ID doesn't exist" })
         }
 
+
+        // Check if the provided user already has a credit card and check its expiration date 
         const existingVCC = await vccRepository.getCreditCardByUser(userId)
         const now = new Date();
 
@@ -29,24 +30,28 @@ export async function getVCC(req, res) {
             return res.status(400).json({ message: "your generated virtual card have not expired yet", 'VCC': existingVCC })
         }
         // If the virtual credit card exists but has already expired
-        else if (existingVCC && existingVCC.expirationDate < now) {
+        else if (existingVCC && existingVCC.expirationDate <= now) {
             await vccRepository.deleteVCC(existingVCC.id)
         }
 
+
+        // validate the amount entered from the request
         if (amount < 0) {
-            return res.status(400).json({ message: "amount entered is invalid" })
+            return res.status(400).json({ error: "amount entered is invalid" })
         }
 
+        // check if the current user's balance is sufficient for the card generation
+        if (amount > user.balance) {
+            return res.status(400).json({ error: "Insufficient balance" })
+        }
+
+        // Send a request to an external api to generate a virtual credit card number according to the type chosen by the user
         const apiResponse = await fetchCCData(visa_type)
 
         // successful response
         if (!apiResponse.isAxiosError) {
 
-
-            const fullname = `${user.firstName} ${user.LastName}`
             const ccnumber = apiResponse.data[0].cardNumber
-            const ccType = visa_type.toLowerCase();
-
 
             // ensures that generated virtual credit card is unique
             let checkExistingCCNumber;
@@ -61,17 +66,22 @@ export async function getVCC(req, res) {
             } while (checkExistingCCNumber);
 
 
+            const fullname = `${user.firstName} ${user.LastName}`
+            const ccType = visa_type.toLowerCase();
+
             // Set expiration date to 24 hours from now
             const expirationDate = new Date();
             expirationDate.setHours(expirationDate.getHours() + 24);
 
+
+            // create a random cvc according to the provided credit card type
             let cvc;
-            if (ccType === 'visa' || ccType === 'mastercard') {
-                cvc = Math.floor(Math.random() * 900) + 100;
-            }
-            else if (ccType === 'amex' || ccType == 'jcb') {
-                cvc = Math.floor(Math.random() * 9000) + 1000;
-            }
+
+            // Visa, Mastercard cards have a 3-digit CVC code
+            if (ccType === 'visa' || ccType === 'mastercard') { cvc = Math.floor(Math.random() * 900) + 100; }
+
+            // Amex, JCB cards have a 4-digit CVC code
+            else if (ccType === 'amex' || ccType == 'jcb') { cvc = Math.floor(Math.random() * 9000) + 1000; }
 
             const VCCData = {
                 cardNumber: ccnumber,
@@ -85,7 +95,7 @@ export async function getVCC(req, res) {
             };
 
             const createdCVV = await vccRepository.createVCC(VCCData);
-            console.log(createdCVV)
+
             return res.status(201).json({ 'VCC': createdCVV });
         } else {
             throw new Error(apiResponse.message)
